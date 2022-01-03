eff8816a453d4e3b0c3e7f5d2fb38e3956763830
David Ström <david.strom88@gmail.com>
2022-01-03 19:56:56 +0100

    Add cart reducer and action, plus hocking them up to add to cart buttons

--- App.js
+++ App.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import { enableScreens } from 'react-native-screens';
 
 import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
 import ShopNavigator from './navigation/ShopNavigator';
+
+enableScreens();
 
 const rootReducer = combineReducers({
   products: productsReducer,
+  cart: cartReducer,
 });
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function App() {
+  const [isFontsLoaded, setIsFontsLoaded] = useState(false);
+
+  if (!isFontsLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setIsFontsLoaded(true)}
+        onError={err => console.log(err)}
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -7,14 +7,29 @@ import {
   Button,
   ScrollView,
 } from 'react-native';
+import Colors from '../../constants/colors';
+import { useDispatch } from 'react-redux';
+import * as cartActions from '../../store/actions/cart';
 
 const ProductDetailScreen = props => {
-  const product = props.navigation.getParam('product');
+  const selectedProduct = props.navigation.getParam('product');
+  const dispatch = useDispatch();
+
   return (
-    <View>
-      <Text>The product detail Screen!</Text>
-      <Text>{product.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button
+          color={Colors.primary}
+          title='Add to Cart'
+          onPress={() => {
+            dispatch(cartActions.addToCart(selectedProduct));
+          }}
+        />
+      </View>
+      <Text style={styles.price}>${selectedProduct.price}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
@@ -24,6 +39,28 @@ ProductDetailScreen.navigationOptions = navData => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20,
+    fontFamily: 'open-sans-bold',
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20,
+    fontFamily: 'open-sans',
+  },
+});
 
 export default ProductDetailScreen;
